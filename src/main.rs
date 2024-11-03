5e580b80d1b3df1747da56399c13551135fdf649
MrDulfin <dulfinaminator@gmail.com>
2024-11-03 11:18:50 -0500

    added config

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use std::{env::{self, args}, fs, hash::Hash, io::{Read, Write}, mem, path::{Path, PathBuf}, sync::atomic::AtomicI64, time::Duration};
+use std::{env::{args, Args}, fs, io::{Read, Write}, path::Path};
 
 use chrono::{DateTime, Utc};
-use moro_local::async_scope;
-use reqwest::{multipart::Part, Body, Client};
+
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
-use tokio::{fs::File, io::AsyncReadExt, sync::Mutex, time::sleep};
-use tokio_util::codec::BytesCodec;
+use tokio::{fs::File, io::AsyncReadExt, sync::Mutex};
 use uuid::Uuid;
+use colored::Colorize;
+
+const DEBUG_CONFIG: &str = "test/config.toml";
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let mut args= args();
-    let mut config = Config::open("test/config.toml").unwrap();
+    let mut config = Config::open().unwrap();
 
     if args.len() < 2 {
-        println!("{args:?}");
         help();
         return Ok(());
     }
@@ -24,11 +24,14 @@ async fn main() -> Result<(), String> {
 
     // TODO: load default
 
-    args.next();
+    let exec_name = Path::new(&args.next().unwrap()).file_name().unwrap().to_str().unwrap().to_string();
     if let Some(arg) = args.next() {
         match arg.to_lowercase().as_str() {
+            x if x != "set" && url.is_empty() => {
+                println!("Please set a url with {}.", format!("{} set url <url>", exec_name).as_str().bold());
+            }
             "upload" => {
-                if let Some(mut path) = args.next() {
+                if let Some(path) = args.next() {
                     let mut first = true;
                     let mut files: Vec<(tokio::fs::File, Option<String>)> = vec![];
                     loop {
@@ -46,7 +49,7 @@ async fn main() -> Result<(), String> {
                                         files.last_mut().unwrap().1 = Some(x);
                                         continue;
                                     },
-                                    _ => arg.to_lowercase()
+                                    _ => arg
                                 },
                                 None => break
                             }
@@ -67,11 +70,15 @@ async fn main() -> Result<(), String> {
                             s.spawn(async {
                                 let file = upload_file(name.unwrap(), file, &client, url.clone(), None, &config).await.unwrap();
                                 results.lock().await.push(file);
-                            }).await
+                            });
                         }
                     }).await;
 
-                    dbg!(results);
+                    let res = results.into_inner();
+
+                    for file in res {
+                        println!("\nname: {}\npath: {url}/f/{}", file.name, file.mmid.0);
+                    }
 
                     return Ok(());
                 } else {
@@ -80,15 +87,26 @@ async fn main() -> Result<(), String> {
             },
             "download" => {todo!()}
             "files" => {todo!()}
-            "login" => {todo!()}
+            "set" => {
+                set(args, config).await
+            }
             "info" => {
                 let client = Client::new();
-                let info = client.get(format!("{url}/info")).basic_auth(config.login.as_ref().unwrap().user.clone(), config.login.as_ref().unwrap().pass.clone()).send().await.unwrap().json::<ServerInfo>().await.unwrap();
-                println!("{:?}", info);
+                let info = client.get(
+                    format!("{url}/info"))
+                    .basic_auth(
+                        config.login.as_ref().unwrap().user.clone(),
+                        config.login.as_ref().unwrap().pass.clone()
+                    ).send()
+                    .await
+                    .unwrap()
+                    .json::<ServerInfo>()
+                    .await
+                    .unwrap();
+
                 config.info = Some(info);
-                config.save("test/config.toml").unwrap();
+                config.save().unwrap();
             }
-            "auth" => {}
             x => {println!(r#"ERROR: "{x}" is an invalid keyword"#)}
         }
     }
@@ -105,7 +123,7 @@ async fn upload_file(name: String, file: File, client: &Client, url: String, dur
         client.post(format!("{url}/upload/chunked/"))
                 .json(
                     &ChunkedInfo {
-                        name,
+                        name: name.clone(),
                         size: bytes.len() as u64,
                         expire_duration: if let Some(dur) = duration { dur } else { config.info.as_ref().unwrap().default_duration.clone() as i64 }
                     }
@@ -117,13 +135,13 @@ async fn upload_file(name: String, file: File, client: &Client, url: String, dur
                 .await
                 .unwrap()
     };
-    dbg!(status, message, chunk_size);
+    // dbg!(status, message, chunk_size);
 
 
     let chunks = bytes.chunks(chunk_size as usize);
+    let len = chunks.len();
 
     for (i, chunk) in chunks.enumerate() {
-        println!("{i}");
         let url = url.clone();
             let chunk = chunk.to_vec();
             let offset = chunk_size * i as u64;
@@ -138,10 +156,15 @@ async fn upload_file(name: String, file: File, client: &Client, url: String, dur
             .text()
             .await
             .unwrap();
-            println!("{i}: {res:?}");
+            // dbg!(res);
+
+            let completion: u8 = ((i + 1) as f32 / len as f32 * 100.0) as u8;
+            if completion == 100 {
+                println!("🎊 {name} upload complete! 🎊");
+            } else {
+                println!("{name}: {completion}% uploaded");
+            }
     }
-
-
     Ok(
         client.get(format!("{url}/upload/chunked/{uuid}?finish"))
         .basic_auth(user, pass)
@@ -151,6 +174,66 @@ async fn upload_file(name: String, file: File, client: &Client, url: String, dur
         .await
         .unwrap()
     )
+}
+
+async fn set(mut args: Args, mut config: Config) {
+    loop {
+        if let Some(arg) = args.next() {
+            match arg.to_lowercase().as_str() {
+                x if x == "username" || x == "user" || x == "-u" => {
+                    if let Some(arg) = args.next() {
+                        let login = &mut config.login;
+                        if login.is_some() {
+                            config.login.as_mut().unwrap().user = arg.clone();
+                        } else {
+                            config.login = Some(
+                                Login {
+                                    user: arg.clone(),
+                                    pass: None
+                                }
+                            );
+                        }
+                        config.save().unwrap();
+                        println!("Username set to {}", arg.bold())
+                    } else {
+                        println!("Pleae insert a username");
+                    }
+                }
+                x if x == "password" || x == "pass" || x == "-p" => {
+                    if let Some(arg) = args.next() {
+                        let login = &mut config.login;
+                        if login.is_some() {
+                            config.login.as_mut().unwrap().pass = Some(arg.clone());
+                        } else {
+                            config.login = Some(
+                                Login {
+                                    user: String::new(),
+                                    pass: Some(arg.clone())
+                                }
+                            );
+                        }
+                        config.save().unwrap();
+                        println!("Password set to {}", arg.bold())
+                    } else {
+                        println!("Please insert a password");
+                    }
+                }
+                "url" => {
+                    if let Some(arg) = args.next() {
+                        let arg = arg.trim().trim_end_matches('/').to_string();
+                        config.url = arg.clone();
+                        config.save().unwrap();
+                        println!("Url set to {}", arg.bold())
+                    } else {
+                        println!("Please insert a url");
+                    }
+                },
+                x => println!("Setting \"{x}\" is not a valid setting.\nSettings: username (-u), password (-p), url")
+            }
+        } else {
+            break;
+        }
+    }
 }
 
 fn help() {
@@ -230,7 +313,8 @@ struct Login {
     pass: Option<String>
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Default)]
+#[serde(default)]
 struct Config {
     url: String,
     login: Option<Login>,
@@ -238,9 +322,9 @@ struct Config {
 }
 
 impl Config {
-    fn open(path: &str) -> Result<Self, ()> {
+    fn open() -> Result<Self, Box<dyn std::error::Error>> {
         let c = if cfg!(debug_assertions) {
-            if let Ok(str) = fs::read_to_string(path) {
+            if let Ok(str) = fs::read_to_string(DEBUG_CONFIG) {
                 str
             } else {
                 let c = Config {
@@ -248,18 +332,73 @@ impl Config {
                     login: None,
                     info: None,
                 };
-                c.save(path).unwrap();
+                c.save().unwrap();
                 return Ok(c);
             }
         } else {
-            unimplemented!()
+            if let Some(dir) = directories::ProjectDirs::from("", "Dangoware", "confetti_cli") {
+                let path = dir.config_dir();
+                fs::create_dir(path).or_else(|err| {
+                    if err.kind() == std::io::ErrorKind::AlreadyExists {
+                        Ok(())
+                    } else {
+                        Err(err)
+                    }
+                })?;
+
+                let mut buf: String = String::new();
+
+                fs::OpenOptions::new()
+                .create(true)
+                .write(true)
+                .read(true)
+                .open(path.join("config.toml"))
+                .unwrap()
+                .read_to_string(&mut buf)
+                .unwrap();
+
+                if buf.is_empty() {
+                    let c = Config {
+                        url: String::new(),
+                        login: None,
+                        info: None,
+                    };
+                    c.save().unwrap();
+
+                    // dbg!(path);
+                    return Ok(c);
+                } else {
+                    buf
+                }
+            } else {
+                panic!("no project dir?")
+            }
         };
 
         Ok(toml::from_str::<Config>(c.as_str()).unwrap())
     }
 
-    fn save(&self, path: &str) -> Result<(), ()> {
-        fs::OpenOptions::new().create(true).write(true).open(path).unwrap().write_all(toml::to_string(self).unwrap().as_bytes()).unwrap();
+    fn save(&self) -> Result<(), ()> {
+        let path = if cfg!(debug_assertions) {
+            DEBUG_CONFIG.to_string()
+        } else {
+            if let Some(dir) = directories::ProjectDirs::from("", "Dangoware", "confetti_cli") {
+                let path = dir.config_dir();
+                fs::create_dir(path).or_else(|err| {
+                    if err.kind() == std::io::ErrorKind::AlreadyExists {
+                        Ok(())
+                    } else {
+                        Err(err)
+                    }
+                }).unwrap();
+                let x = path.join("config.toml");
+                x.clone().to_str().unwrap().to_string()
+            } else {
+                panic!("no project dir?")
+            }
+        };
+
+        fs::OpenOptions::new().create(true).write(true).truncate(true).open(path).unwrap().write_all(toml::to_string(self).unwrap().as_bytes()).unwrap();
         Ok(())
     }
 }
